b9c7a226aa3f31cbb80cc65966193f9d24739d03
Elwin <elwin.carlos09@gmail.com>
2021-04-12 12:31:28 +0100

    stub out layout for main page

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+    return (
+        <>
+            <header>
+                <h1>Welcome to my Music App!</h1>
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
+    );
+}
+
+export default App;
