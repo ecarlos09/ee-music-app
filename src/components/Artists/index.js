3ca1403e76c8527b5ad2634dee3a9663ff63a63a
Elwin <elwin.carlos09@gmail.com>
2021-04-13 11:28:01 +0100

    nest and rename Artists component inside a new folder and update imports

---
+++ src/components/Artists/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Artists = () => {
+    const [artist, setArtist] = useState([
+        { name: "Josh Groban", link: "https://www.youtube.com/channel/UCIzJyWKY_d3540fhUa2eJUA" },
+        { name: "Journey", link: "https://www.youtube.com/watch?v=fTkHFQC3wow&list=RDEMaN44Jp63Y-AyoHV9WoMk6g&start_radio=1" },
+        { name: "Queen", link: "https://www.youtube.com/channel/UCiMhD4jzUqG-IgPzUmmytRQ" }
+    ])
+
+    const renderRows = () => {
+        return artist.map(a => (
+            <tr>
+                <td>{a.name}</td>
+                <td><a href={a.link}>Link</a></td>
+            </tr>                
+        ));
+    };
+
+    return (
+        <table style = {{border: "1px solid black", width:"100vw", textAlign: "center"}}>
+            <thead>
+                <tr>
+                    <th>Artist</th>
+                    <th>YouTube content</th>
+                </tr>
+            </thead>
+            <tbody>
+                { renderRows() }
+            </tbody>
+        </table>
+    );
+};
+
+export default Artists;
---
+++ src/components/Artists/test.js
@@ -0,0 +1,17 @@
+// Import screen watcher
+import { render, screen, within } from '@testing-library/react';
+
+// Import our app file
+import Artists from '.';
+
+describe('Artists', () => {
+    beforeEach(() => {
+        // Render the Artists component before each test
+        render(<Artists />);
+    })
+
+    it('renders a table', () => {
+        const table = screen.queryByRole('table');
+        expect(table).toBeInTheDocument();
+    })
+})
