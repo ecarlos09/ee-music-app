15bdf4836d271e8583dfce6ebdf28a686f4cc89a
Elwin <elwin.carlos09@gmail.com>
2021-04-13 12:40:08 +0100

    add LikeButton to list of component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Artists } from './Artists';
 export { default as Singles } from './Singles';
+export { default as LikeButton } from './LikeButton';
