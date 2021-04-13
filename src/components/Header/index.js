23f1f08b3d5874c3296cf23e1539dea55db261cf
Elwin <elwin.carlos09@gmail.com>
2021-04-13 11:28:44 +0100

    nest and rename Heading component inside a new folder and update imports

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <section className="heading">
+            <h1>Choonsic Music</h1>
+            <h5>A place for you to choose some sick choons ...</h5>
+        </section>
+    )
+
+}
+
+export default Heading;
