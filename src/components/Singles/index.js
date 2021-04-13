9b5d9571e10f0796f0b7f962a22c3df100a5e13e
Elwin <elwin.carlos09@gmail.com>
2021-04-13 12:43:52 +0100

    remove liked property from singles

--- src/components/Singles/index.js
+++ src/components/Singles/index.js
@@ -4,12 +4,12 @@ import { LikeButton } from '..';
 
 const Singles = () => {
     const [single, setSingle] = useState([
-        { title: "To Where You Are", artist: "Josh Groban", video: "https://www.youtube.com/watch?v=PXnKt3Wclzo", liked: false },
-        { title: "Bridge Over Troubled Water", artist: "Josh Groban", video: "https://www.youtube.com/watch?v=utTYvc2XsLY", liked: false },
-        { title: "Don't Stop Believin'", artist: "Journey", video: "https://www.youtube.com/watch?v=1k8craCGpgs", liked: false },
-        { title: "Faithfully", artist: "Journey", video: "https://www.youtube.com/watch?v=OMD8hBsA-RI", liked: false },
-        { title: "Bohemian Rhapsody", artist: "Queen", video: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ", liked: false },
-        { title: "Don't Stop Me Now", artist: "Queen", video: "https://www.youtube.com/watch?v=HgzGwKwLmgM", liked: false }        
+        { title: "To Where You Are", artist: "Josh Groban", video: "https://www.youtube.com/watch?v=PXnKt3Wclzo" },
+        { title: "Bridge Over Troubled Water", artist: "Josh Groban", video: "https://www.youtube.com/watch?v=utTYvc2XsLY" },
+        { title: "Don't Stop Believin'", artist: "Journey", video: "https://www.youtube.com/watch?v=1k8craCGpgs" },
+        { title: "Faithfully", artist: "Journey", video: "https://www.youtube.com/watch?v=OMD8hBsA-RI" },
+        { title: "Bohemian Rhapsody", artist: "Queen", video: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ" },
+        { title: "Don't Stop Me Now", artist: "Queen", video: "https://www.youtube.com/watch?v=HgzGwKwLmgM" }        
     ])
 
     const renderSingles = () => {
