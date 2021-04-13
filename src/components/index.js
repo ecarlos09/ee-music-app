ddf21e229cdd5fa215ff0d934c9723cb370db11d
Elwin <elwin.carlos09@gmail.com>
2021-04-13 17:41:55 +0100

    import/export Lyrics

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Artists } from './Artists';
 export { default as Singles } from './Singles';
 export { default as LikeButton } from './LikeButton';
+export { default as Lyrics } from './Lyrics';
