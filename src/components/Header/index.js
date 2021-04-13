ab27e572faca575519f929fb73f379bdac92ec24
Elwin <elwin.carlos09@gmail.com>
2021-04-13 11:58:43 +0100

    add logo and place heading elements inside divs

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,13 @@ import React from 'react';
 const Heading = () => {
     return (
         <section className="heading">
-            <h1>Choonsic Music</h1>
-            <h5>A place for you to choose some sick choons ...</h5>
+            <div id="title-icon">
+                <i class="fas fa-record-vinyl"></i>
+            </div>
+            <div id="title">
+                <h1>Choonsic Music</h1>
+                <h5>A place for you to choose some sick choons ...</h5>
+            </div>
         </section>
     )
 
