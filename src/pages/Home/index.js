fa5ff1519a65eaf90fedb585c8a3b5608f5d0d1a
Elwin <elwin.carlos09@gmail.com>
2021-04-14 18:09:03 +0100

    add random number generator to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { Greeting } from '../../components';
+import { Greeting, RandNumGen } from '../../components';
 
 const Home = () => {
     return (
         <>
             <h1>Welcome to our site!</h1>
             <Greeting />
+            <h5>Need a random number?</h5>
+            <RandNumGen />
         </>
     )
 }
