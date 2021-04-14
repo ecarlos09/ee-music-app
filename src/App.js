3cf27a2a8572b0dfdaad091febf6ff97919c6031
Elwin <elwin.carlos09@gmail.com>
2021-04-14 11:48:20 +0100

    stub out routing and routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import { Header, Footer } from './layout';
-import { Artists, Singles } from './components';
+import { Home, About, Artists, Singles, Support, Contact } from './pages';
 
 function App() {
     return (
         <>
+            <nav>
+
+            </nav>
+            
             <header>
                 <Header />
             </header>
 
-            <main>
-                <Artists />
-                <Singles />
-            </main>
+            <Switch>
+                <Route exact path ="/" component={Home} />
+                <Route path ="/about" component={About} />
+                <Route path ="/artists" component={Artists} />
+                <Route path ="/singles" component={Singles} />
+                <Route path ="/support" component={Support} />
+                <Route path ="/contact" component={Contact} />
+            </Switch>
+
 
             <footer>
                 <Footer />
