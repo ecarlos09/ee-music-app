7aa87fa1bfc3d995495b7880c5d7aa4906523027
Elwin <elwin.carlos09@gmail.com>
2021-04-14 07:57:04 +0100

    create layout folder with footer component and move header folder inside

---
+++ src/layout/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <section className="heading">
+            <div id="title-icon">
+                <i class="fas fa-record-vinyl"></i>
+            </div>
+            <div id="title">
+                <h1>Choonsic Music</h1>
+                <h5>A place for you to choose some sick choons ...</h5>
+            </div>
+        </section>
+    )
+
+}
+
+export default Heading;
