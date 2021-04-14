d8fb719339b68e9a2b77e047e2f62fc82a56bd12
Elwin <elwin.carlos09@gmail.com>
2021-04-14 12:25:15 +0100

    stub out back button

---
+++ src/components/BackButton/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const BackButton = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default BackButton;
